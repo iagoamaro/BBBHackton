265d13fcf7793937caa7e1e809b1066bfd623ea3
Iago A <iagoamaro@hotmail.com>
2017-11-08 22:00:53 -0200

    Add project files.

---
+++ BBBHackton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BBBHackton.Startup))]
+namespace BBBHackton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
